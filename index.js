89853c6f8e37ac0b077f331af4c01920ecf52bf5
chiheb <chihababiza9@gmail.com>
2024-06-27 15:12:35 +0100

    .

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.put('/edit/:id', controllers.updatePost); // Route to handle update
 //  Delete request
 app.delete('/delete/:id', controllers.deletePost);
 
-mongoose.connect('mongodb+srv://userforBlog:COjDVyTbrxw6RkpS@cluster0.9l3zkjy.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://3vWL3pk17lvhh79K:3vWL3pk17lvhh79K@cluster0.yu2bxuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => {
         app.listen(port, () => {
             console.log(`Example app listening on port ${port}`)
